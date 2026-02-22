f65ca5c864005a190f99a36a2dd12e2727f42443
Sabin Puiu <puiumihaisabin@gmail.com>
2026-02-22 19:30:38 +0200

    feat: expose public API from package entry point

--- index.ts
+++ index.ts
@@ -1,1 +1,17 @@
-console.log("Hello via Bun!");
+export { Fumi } from './src/fumi.ts'
+export {
+  SMTPError,
+  type FumiOptions,
+  type Plugin,
+  type Middleware,
+  type Session,
+  type Address,
+  type Envelope,
+  type Credentials,
+  type ConnectContext,
+  type AuthContext,
+  type MailFromContext,
+  type RcptToContext,
+  type DataContext,
+  type CloseContext,
+} from './src/types.ts'
