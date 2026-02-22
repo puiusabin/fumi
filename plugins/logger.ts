c49b1d76cbe1060b1fd91cc338f34ec9b30eb60c
Sabin Puiu <puiumihaisabin@gmail.com>
2026-02-22 19:53:35 +0200

    feat: add logger plugin

---
+++ plugins/logger.ts
@@ -0,0 +1,31 @@
+import type { Plugin } from "../index.ts";
+
+/**
+ * Logs each SMTP phase to stdout.
+ *
+ *
+ * @example
+ * app.use(logger())
+ */
+export function logger(): Plugin {
+  return (app) => {
+    app.onConnect(async (ctx, next) => {
+      console.log(`[connect] ${ctx.session.remoteAddress}`);
+      await next();
+    });
+
+    app.onMailFrom(async (ctx, next) => {
+      console.log(`[mail from] ${ctx.address.address}`);
+      await next();
+    });
+
+    app.onRcptTo(async (ctx, next) => {
+      console.log(`[rcpt to] ${ctx.address.address}`);
+      await next();
+    });
+
+    app.onClose(async (ctx) => {
+      console.log(`[close] ${ctx.session.remoteAddress}`);
+    });
+  };
+}
