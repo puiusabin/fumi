b19fceb929467f25303675e63e3b61d5b878c565
Sabin Puiu <puiumihaisabin@gmail.com>
2026-02-24 15:01:16 +0200

    feat(docs): register Step and Steps MDX components

--- docs/mdx-components.tsx
+++ docs/mdx-components.tsx
@@ -1,3 +1,4 @@
+import { Step, Steps } from 'fumadocs-ui/components/steps';
 import { Tab, Tabs } from 'fumadocs-ui/components/tabs';
 import defaultMdxComponents from 'fumadocs-ui/mdx';
 import type { MDXComponents } from 'mdx/types';
@@ -5,6 +6,8 @@ import type { MDXComponents } from 'mdx/types';
 export function getMDXComponents(components?: MDXComponents): MDXComponents {
   return {
     ...defaultMdxComponents,
+    Step,
+    Steps,
     Tab,
     Tabs,
     ...components,
